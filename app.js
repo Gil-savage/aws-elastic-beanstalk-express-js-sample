725784c28f4040c117d776d894fe762e579c263d
Gil-savage <63843938+Gil-savage@users.noreply.github.com>
2020-12-30 21:09:23 +0200

    Update app.js
    Adding More Words

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World From Gil Sabag !'));
+app.get('/', (req, res) => res.send('Hello World From Gil Sabag. Whatts up !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
